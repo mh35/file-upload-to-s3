97bed32a6c6726a9f87fe4b62bd01909a886fb70
MH35 <mh35jp@gmail.com>
2025-04-02 08:28:56 +0900

    feat: :sparkles: Plugin header

---
+++ file-upload-to-s3.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: File Upload to S3
+ * Plugin URI: https://github.com/mh35/file-upload-to-s3
+ * Description: Upload uploaded files to Amazon S3 bucket.
+ * Version: 0.0.1
+ * Requires PHP: 8.1
+ * Author: MH35
+ * Author URI: https://github.com/mh35
+ * Text Domain: file-upload-to-s3
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * 
+ * @package file-upload-to-s3
+ * @author MH35
+ * @license GPL-2.0+
+ */
